56b95ffbdd791ee232f876c994aa9cf820ca75cb
Dante Ferreira <dante.tf5@gmail.com>
2026-05-20 22:37:15 -0300

    Fix window borders, Waybar duplication and adjust opacity settings

--- UserScripts/RainbowBorders.sh
+++ UserScripts/RainbowBorders.sh
@@ -13,4 +13,4 @@ c4=$(grep '^\$color12 ' "$CONF_FILE" | awk -F '=' '{print $2}' | tr -d ' ')
 c5=$(grep '^\$color13 ' "$CONF_FILE" | awk -F '=' '{print $2}' | tr -d ' ')
 c6=$(grep '^\$color14 ' "$CONF_FILE" | awk -F '=' '{print $2}' | tr -d ' ')
 
-hyprctl keyword general:col.active_border $c1 $c2 $c3 $c4 $c5 $c6 $c1 $c2 $c3 $c4 270deg
+hyprctl keyword general:col.active_border "$c1 $c2 $c3 $c4 $c5 $c6 $c1 $c2 $c3 $c4 270deg"
